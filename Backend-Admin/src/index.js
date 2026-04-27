62c9ad601616d49b5a28ca8186ca9d89cad68984
Bharat Madan <bharatmadan2016@gmail.com>
2026-04-27 20:18:31 +0530

    index.js update

--- Backend-Admin/src/index.js
+++ Backend-Admin/src/index.js
@@ -1,26 +1,19 @@
-import express from "express"
 import connectDB from "./config/mongo.config.js";
-import dotenv from "dotenv"
-import app from "./app.js"
-dotenv.config()
-// const app = express();
-app.use(express.json());
+import dotenv from "dotenv";
+import app from "./app.js";
 
-
-app.get("/",(req,res)=>{
-    res.send("Pharmacy app")
-})
+dotenv.config();
 
 const PORT = process.env.PORT || 9000;
 
-// app.listen(PORT,()=>{
-//     console.log(`This App is running at ${PORT}`)
-// })
+// Start server only after DB connects
 connectDB()
-.then(()=>{
-    app.listen(8005, "0.0.0.0", () => {
-  console.log("Server is running on network at port 8005");
+.then(() => {
+    app.listen(PORT, "0.0.0.0", () => {
+        console.log(`🚀 Server running on port ${PORT}`);
+    });
+})
+.catch((err) => {
+    console.error(" DB connection error:", err);
 });
-}).catch((err) => {
-    console.log("DB connection error ", err);
-});
+
